e7aa97ad2ebd8fd23b65545f721d9d976ee3c16a
webdevdjm <webdevdjm@gmail.com>
2021-03-20 17:19:21 -0400

    Added 2 more...

---
+++ Javascript/8kyu/feast-of-many-beasts.js
@@ -0,0 +1,18 @@
+// All of the animals are having a feast! Each animal is bringing one dish. There is just one rule: the dish must start and end with the same letters as the animal's name. For example, the great blue heron is bringing garlic naan and the chickadee is bringing chocolate cake.
+
+// Write a function feast that takes the animal's name and dish as arguments and returns true or false to indicate whether the beast is allowed to bring the dish to the feast.
+
+// Assume that beast and dish are always lowercase strings, and that each has at least two letters. beast and dish may contain hyphens and spaces, but these will not appear at the beginning or end of the string. They will not contain numerals.
+
+function feast(beast, dish) {
+  //your function here
+    const beast1 = beast[0]
+    const dish1 = dish[0]
+    const beastLast = beast[beast.length - 1]
+    const dishLast = dish[dish.length - 1]
+    if (beast1 === dish1 && beastLast === dishLast) {
+      return true
+    } else {
+      return false
+    }
+  }
